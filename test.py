fd1b34ac9918d33ce4e1772fad72518ed3efa59f
SeunghoShin93 <tmdghshin@gmail.com>
2020-07-19 23:43:38 +0900

    m

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import livepopulartimes
 import pprint
 
-result = livepopulartimes.get_populartimes_by_PlaceID("AIzaSyDO7MnUwQv0Vdsq8db6PbNxOAlCjN20bF8","ChIJ-5cOJL-PaDURix8BdKuOMiA")
+result = livepopulartimes.get_populartimes_by_PlaceID("apikey","ChIJ-5cOJL-PaDURix8BdKuOMiA")
 
 pprint.pprint(result)
