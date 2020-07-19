da927170cc78a0036a5ebd71d61964cd8914b7fb
SeunghoShin93 <tmdghshin@gmail.com>
2020-07-19 23:33:54 +0900

    테스트

---
+++ test.py
@@ -0,0 +1,6 @@
+import livepopulartimes
+import pprint
+
+result = livepopulartimes.get_populartimes_by_PlaceID("AIzaSyDO7MnUwQv0Vdsq8db6PbNxOAlCjN20bF8","ChIJ-5cOJL-PaDURix8BdKuOMiA")
+
+pprint.pprint(result)
