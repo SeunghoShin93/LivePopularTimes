fdd3410d6f1fdb063ec7228f4042c6bab53eb0b3
SeunghoShin93 <tmdghshin@gmail.com>
2020-07-20 09:36:15 +0900

    Decoupled Secret Key added

--- test.py
+++ test.py
@@ -1,6 +1,9 @@
 import livepopulartimes
 import pprint
+from decouple import config
 
-result = livepopulartimes.get_populartimes_by_PlaceID("apikey","ChIJ-5cOJL-PaDURix8BdKuOMiA")
+SECRET_KEY = config('API_KEY')
+
+result = livepopulartimes.get_populartimes_by_PlaceID(SECRET_KEY,"ChIJ-5cOJL-PaDURix8BdKuOMiA")
 
 pprint.pprint(result)
